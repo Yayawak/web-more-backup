bee4a809944e0bc790f91c643bd83cefb3d8fe32
Siwakorn Mahakan <siwakorn.m@nipa.cloud>
2022-06-22 22:54:36 +0700

    slideshow frame

--- components/Slideshow/Button.tsx
+++ components/Slideshow/Button.tsx
@@ -8,7 +8,23 @@ const sideToText: { [key: string]: string } = {
 }
 
 const SlideshowButton = ({ side }: ButtonProps) => {
-  return <div className="w-[75px] h-[124px] bg-black">{sideToText[side]}</div>
+  return (
+    <>
+      <div
+        className={`slideshow-button w-[75px] h-[124px] text-white text-[30px] absolute flex justify-center items-center ${
+          side === "right" && "right-0 "
+        } cursor-pointer select-none`}
+      >
+        {sideToText[side]}
+      </div>
+
+      <style jsx lang="scss">{`
+        .slideshow-button {
+          background-color: rgba(0, 0, 0, 0.7);1
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default SlideshowButton
--- components/Slideshow/Slideshow.tsx
+++ components/Slideshow/Slideshow.tsx
@@ -1,16 +1,23 @@
 import SlideshowButton from "./Button"
 
 interface SlideshowProps {
-  image: string[]
+  image?: string[]
 }
 
 const Slideshow = ({ image }: SlideshowProps) => {
   return (
     <>
-      <div className="relative w-full h-full">
+      <div className="relative w-full h-full bg-black">
         <SlideshowButton side="left" />
         <SlideshowButton side="right" />
       </div>
+
+      <style jsx lang="scss">{`
+        :global(.slideshow-button) {
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      `}</style>
     </>
   )
 }
