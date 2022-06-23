af3b6f1d44bb1c7fa2a6524af06a04b7a5fd6599
Siwakorn Mahakan <siwakorn.m@nipa.cloud>
2022-06-23 21:29:24 +0700

    feat: format date

--- components/Cards/News/NewsCard.tsx
+++ components/Cards/News/NewsCard.tsx
@@ -1,3 +1,5 @@
+import * as datefns from 'date-fns'
+
 interface INewsCardProps {
   width: number
   height: number
@@ -16,14 +18,21 @@ const NewsCard = ({
   date,
 }: INewsCardProps) => {
   return (
-    <div
-      className={`drop-shadow-lg w-[${width}px] h-[${height}px] bg-white rounded-[20px]`}
-    >
-      <span>{imageUrl}</span>
-      <span>{title}</span>
-      <span>{description}</span>
-      <span>{date?.toString()}</span>
-    </div>
+    <>
+      <div className={`news-card drop-shadow-lg bg-white rounded-[20px]`}>
+        <span>{imageUrl}</span>
+        <span>{title}</span>
+        <span>{description}</span>
+        {date && <span>{datefns.format(date, 'dd MMM yyyy')}</span>}
+      </div>
+
+      <style jsx>{`
+        .news-card {
+          width: ${width}px;
+          height: ${height}px;
+        }
+      `}</style>
+    </>
   )
 }
 
