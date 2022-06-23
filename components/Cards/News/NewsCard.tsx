01dd1c86b6141b55b6fd3b480d1d9399c8749a32
Siwakorn Mahakan <siwakorn.m@nipa.cloud>
2022-06-23 21:53:15 +0700

    feat: add news card details

--- components/Cards/News/NewsCard.tsx
+++ components/Cards/News/NewsCard.tsx
@@ -19,17 +19,34 @@ const NewsCard = ({
 }: INewsCardProps) => {
   return (
     <>
-      <div className={`news-card drop-shadow-lg bg-white rounded-[20px]`}>
-        <span>{imageUrl}</span>
-        <span>{title}</span>
-        <span>{description}</span>
-        {date && <span>{datefns.format(date, 'dd MMM yyyy')}</span>}
+      <div
+        className={`news-card drop-shadow-lg bg-white rounded-[20px] p-[16px] grid overflow-hidden`}
+      >
+        <div
+          className="news-card-image bg-cover bg-no-repeat bg-center"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        ></div>
+        <div className="news-card-body flex flex-col overflow-hidden mt-[8px]">
+          <div className="mb-[8px]">{title}</div>
+          <div className="news-card-body-description limit-lines">
+            {description}
+          </div>
+        </div>
+        <div className="news-card-footer flex items-center">
+          {date && <span>{datefns.format(date, 'dd MMM yyyy')}</span>}
+        </div>
       </div>
 
       <style jsx>{`
         .news-card {
+          grid-template-rows: 114px auto 50px;
           width: ${width}px;
           height: ${height}px;
+        }
+
+        .news-card-body-description {
+          -webkit-line-clamp: 3;
+          line-clamp: 3;
         }
       `}</style>
     </>
--- components/Cards/News/news-card.stories.tsx
+++ components/Cards/News/news-card.stories.tsx
@@ -5,6 +5,11 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 export default {
   title: 'Common/NewsCard',
   component: NewsCard,
+  argTypes: {
+    date: {
+      control: 'date',
+    },
+  },
 } as ComponentMeta<typeof NewsCard>
 
 const Template: ComponentStory<typeof NewsCard> = (args) => (
@@ -13,8 +18,10 @@ const Template: ComponentStory<typeof NewsCard> = (args) => (
 
 export const Default = Template.bind({})
 Default.args = {
-  imageUrl: '',
-  title: '',
-  description: '',
+  imageUrl:
+    'https://img.online-station.net/_content/2020/1021/178021/gallery/ClariS04_826_587.jpg',
+  title: 'Claris',
+  description:
+    'ClariS is a Japanese pop music duo which formed in 2009 with singers Clara and Alice from Hokkaido, who were in junior high school at the time. The pair began singing covers and submitting them to Japanese video sharing website Niconico between 2009 and 2010.',
   date: new Date(),
 }
