0253dcdc74d7aa09fd6c4d192b8fb7f02658f133
Siwakorn Mahakan <siwakorn.m@nipa.cloud>
2022-06-24 23:08:00 +0700

    feat: add class name props to container

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -3,13 +3,18 @@ import { useMemo } from 'react'
 interface IContainerProps {
   fluid?: boolean
   children?: React.ReactNode
+  className?: string
 }
 
-const Container = ({ fluid = false, children }: IContainerProps) => {
+const Container = ({
+  fluid = false,
+  children,
+  className = '',
+}: IContainerProps) => {
   const getMaxWidth = useMemo(() => (fluid ? 'unset' : '1200px'), [fluid])
   return (
     <>
-      <div className="container">{children}</div>
+      <div className={`container ${className}`}>{children}</div>
 
       <style jsx>{`
         .container {
