a8eae93b448dde68b3009aff8aa5ecaab0d13ed7
Siwakorn Mahakan <siwakorn.m@nipa.cloud>
2022-06-24 20:08:11 +0700

    feat: add base button story

--- components/Buttons/Base/BaseButton.tsx
+++ components/Buttons/Base/BaseButton.tsx
@@ -1,5 +1,53 @@
-const BaseButton = () => {
-  return <></>
+import { Rounded } from '@/types/rounded'
+import { useCallback } from 'react'
+
+interface IBaseButtonProps {
+  roundedType: Rounded
+  rounded: number
+  elevation: boolean
+  children: React.ReactNode
+  backgroundColor: string
+  textColor: string
+}
+
+const BaseButton = ({
+  roundedType = Rounded.NONE,
+  rounded = 0,
+  elevation = true,
+  backgroundColor = '#0A17A7',
+  textColor = 'white',
+  children,
+}: IBaseButtonProps) => {
+  const getRound = useCallback(() => {
+    if (roundedType === Rounded.NONE) {
+      return 0
+    } else if (roundedType === Rounded.FULL) {
+      return 9999
+    } else {
+      return rounded
+    }
+  }, [roundedType, rounded])
+
+  return (
+    <>
+      <button
+        className={`base-button flex justify-center items-center ${
+          elevation && 'drop-shadow-lg'
+        } py-[8px] px-[16px]`}
+      >
+        {children}
+      </button>
+
+      <style jsx>{`
+        .base-button {
+          color: ${textColor};
+          text-transform: uppercase;
+          background-color: ${backgroundColor};
+          border-radius: ${getRound()}px;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default BaseButton
