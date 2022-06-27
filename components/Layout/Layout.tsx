526d5690019c9d6150d9b64cffd830c568e0ad8a
surely <cardeer123.1@gmail.com>
2022-06-27 21:27:40 +0700

    fix: main layout uses flex-col

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
 import { IComponentProps } from '@/types/component-props'
 
 const Layout = ({ children }: IComponentProps) => {
@@ -12,13 +12,14 @@ const Layout = ({ children }: IComponentProps) => {
         <title>Science@KMITL</title>
       </Head>
 
-      <Navbar />
-      <div className="app-main">{children}</div>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="app-main relative flex flex-col grow">{children}</div>
+        <Footer />
+      </div>
 
       <style jsx>{`
         .app-main {
-          position: relative;
           padding-top: var(--navbar-height);
         }
       `}</style>
