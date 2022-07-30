6c095d31b0862f11cbd5767304b1f14d72da40c4
Wutichai Patthaisong <wutichaipatthaisong@gmail.com>
2022-07-30 06:07:37 +0000

    [FE -16 => development]

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 const PageError404: NextPage = () => {
   return (
     <div className="flex flex-col my-auto text-center text-[50px]">
-      <div>404...SORRY</div>
+      <div>404...SORRY we ded</div>
       <div>We couldn&apos;t find that page</div>
     </div>
   )
