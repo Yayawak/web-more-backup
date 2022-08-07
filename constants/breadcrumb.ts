f7bc0f716f229e94eff6f880cd09afc4bd21202b
surely <cardeer.dev@gmail.com>
2022-08-07 23:11:37 +0700

    chore: add jsdoc

--- constants/breadcrumb.ts
+++ constants/breadcrumb.ts
@@ -1,1 +1,13 @@
-export const breadcrumbRoutes = []
+interface IBreadcrumbRoute {
+  path: string
+  name: string // name that will show in the breadcrumb ex. หน้าแรก, ข่าวสาร
+  children: IBreadcrumbRoute[] // sub routes
+}
+
+export const breadcrumbRoutes: IBreadcrumbRoute[] = [
+  {
+    path: '/',
+    name: 'Home',
+    children: [],
+  },
+]
--- helpers/breadcrumb.ts
+++ helpers/breadcrumb.ts
@@ -1,1 +1,7 @@
+/**
+ * A helper function that helps you get breadcrumb array (ex. ['หน้าแรก', 'ข่าวสาร']) and use in any page.
+ * You will have to use Array.join function if you want to display with slash.
+ *
+ * @returns {string[]} an array of route names
+ */
 export const getBreadcrumbArray = () => {}
