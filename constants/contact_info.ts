1aa07f374d26375c1fd4bcf7c48545afd5dd7a83
surely <cardeer.dev@gmail.com>
2022-08-07 23:26:06 +0700

    feat: add contact info constants

---
+++ constants/contact_info.ts
@@ -0,0 +1,15 @@
+interface IContactInfo {
+  name: string
+  engName: string
+  position: string
+  email: string
+}
+
+export const contactInfo: IContactInfo[] = [
+  {
+    name: 'รองศาสตราจารย์ ดร. สุธี  ชุติไพจิตร',
+    engName: 'Associate Professor Dr. Sutee Chutipaijit',
+    position: 'คณบดี / Dean',
+    email: 'sutee.ch@kmitl.ac.th',
+  },
+]
