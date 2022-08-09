5f708ecf6ea6a27a13d318e24eb9cf910080f42f
AcRyu01 <29421achira@gmail.com>
2022-08-09 22:46:47 +0700

    create download page

---
+++ pages/download.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+type Props = {}
+
+const Download: NextPage = ({ }: Props) => {
+    return (
+        <>
+            Download
+        </>
+    );
+}
+
+export default Download
