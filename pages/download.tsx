e52ca47bfef2183cf2f3fce08288f502832efbb7
AcRyu01 <29421achira@gmail.com>
2022-08-10 13:11:45 +0700

    update Select component

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,43 +1,44 @@
+import CustomSelect from '@/components/Download/CustomSelect';
 import { NextPage } from 'next'
+import { useState } from 'react';
 
 type Props = {}
 
 const Download: NextPage = ({ }: Props) => {
+    const [roleSelected, setRoleSelected] = useState(-1);
+    const [docTypeSelected, setDocTypeSelected] = useState(-1);
+    const roleList = [
+        "บุคลากร",
+        "นักศึกษา",
+    ];
+    const docTypeList = [
+        ".doc",
+        ".pdf",
+    ];
+    const rolePlaceholder = "กรุณาเลือกประเภทบุคลากร";
+    const docTypePlaceholder = "กรุณาเลือกประเภทเอกสาร";
     return (
         <div className='bg-[#F2F2F2]'>
-            <div className='2xl:container 2xl:mx-auto min-h-screen pt-9 flex flex-col gap-16 items-center'>
+            <div className='2xl:container 2xl:mx-auto min-h-screen pt-9 flex flex-col gap-6 items-center sm:gap-16'>
                 <h1 className='h6 sm:h1 text-center'>เลือกรายละเอียดเอกสารที่ต้องการ</h1>
-                <div className='w-[50%]'>
-                    <div className='sm:pb-8 h3 font-normal'>
-                        <p className='sm:pb-5'>ประเภทบุคลากร</p>
-                        <div className="mb-3 mx-4 sm:mx-0">
-                            <select className="block w-full bg-white border-2 border-[#212121] hover:border-[#F8560A] px-4 py-2 pr-8 rounded shadow focus:outline-none focus:shadow-outline"
-                                defaultValue={'DEFAULT'}
-                            >
-                                <option value="DEFAULT" disabled>กรุณาเลือกประเภทบุคลากร</option>
-                                <option>บุคลากร</option>
-                                <option>นักศึกษา</option>
-                            </select>
+                <div className='w-[90%] sm:w-[50%]'>
+                    <div className='pb-3 sm:pb-8 subtitle1 sm:h3 font-normal'>
+                        <p className='pb-2 sm:pb-5'>ประเภทบุคลากร</p>
+                        <div className="">
+                            <CustomSelect optionsList={roleList} placeHolder={rolePlaceholder} selectedOption={roleSelected} setSelectedOption={setRoleSelected} />
                         </div>
                     </div>
-                    <div className='h3 font-normal'>
-                        <p className='sm:pb-5'>ประเภทเอกสาร</p>
-                        <div className="mb-3 mx-4 sm:mx-0">
-                            <select className="block w-full bg-white border-2 border-[#212121] hover:border-[#F8560A] px-4 py-2 pr-8 rounded shadow focus:outline-none focus:shadow-outline"
-                                defaultValue={'DEFAULT'}
-                            >
-                                <option value="DEFAULT" disabled>กรุณาเลือกประเภทเอกสาร</option>
-                                <option>.doc</option>
-                                <option>.pdf</option>
-                            </select>
+                    <div className='subtitle1 sm:h3 font-normal'>
+                        <p className='pb-2 sm:pb-5'>ประเภทเอกสาร</p>
+                        <div className="">
+                            <CustomSelect optionsList={docTypeList} placeHolder={docTypePlaceholder} selectedOption={docTypeSelected} setSelectedOption={setDocTypeSelected} />
                         </div>
                     </div>
                 </div>
                 <div className='flex justify-center'>
-                    <button className='bg-[#F8560A] text-white text-[30px] leading-[28px] font-bold py-4 px-24 rounded-xl'>ค้นหา</button>
+                    <button className='bg-[#F8560A] text-white text-[24px] sm:text-[30px] leading-[28px] font-bold py-2 px-12 sm:py-4 sm:px-24 rounded-xl'>ค้นหา</button>
                 </div>
                 <div className='bg-white rounded-xl w-[80%] h-[600px] mb-[474px]'>
-
                 </div>
             </div>
         </div>
