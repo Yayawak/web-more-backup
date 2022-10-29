904277d0c021b0a440df28d1eee6ac823e6350c2
cardeer <cardeer.dev@gmail.com>
2022-10-29 04:35:04 +0000

    [376b6b6 => dev]: fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,10 +2,20 @@ import { NextPage } from 'next'
 
 const PageError404: NextPage = () => {
   return (
-    <div className="flex flex-col my-auto text-center text-[50px]">
-      <div>404...SORRY we ded</div>
-      <div>We couldn&apos;t find that page</div>
-    </div>
+    <>
+      <div className="not-found-container flex flex-col justify-center items-center">
+        <div className="text-[50px]">404</div>
+        <div className="text-[24px]">
+          Sorry, we couldn&apos;t find the page.
+        </div>
+      </div>
+
+      <style scoped lang="scss">{`
+        .not-found-container {
+          height: calc(100vh - var(--navbar-height));
+        }
+      `}</style>
+    </>
   )
 }
 
