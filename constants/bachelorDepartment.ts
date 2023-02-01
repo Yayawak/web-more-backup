2134275bc449f782138ffbf070dcf0cb1a53d1f4
Siwakorn <cardeer.dev@gmail.com>
2023-02-01 18:07:04 +0700

    fix: course-bachelor kdai

--- constants/bachelorDepartment.ts
+++ constants/bachelorDepartment.ts
@@ -5,147 +5,171 @@ import physicDepartment from '@/assets/logos/physicD-logo.png'
 import bioDepartment from '@/assets/logos/bioD-logo.png'
 import comDepartment from '@/assets/logos/comD-logo.png'
 import statDepartment from '@/assets/logos/statD-logo.png'
+import kdaiDepartment from '@/assets/logos/kdai-logo.png'
 
 export const bachelorDepartment: IDepartmentCard[] = [
   {
     logoPath: chemDepartment,
-    name: "ภาควิชาเคมี",
-    describe: "บูรณาการความรู้และทักษะทางวิทยาศาสตร์โดยเน้นหลักการทางเคมีในการคิด วิจัย วิเคราะห์ และแก้ปัญหา",
+    name: 'ภาควิชาเคมี',
+    describe:
+      'บูรณาการความรู้และทักษะทางวิทยาศาสตร์โดยเน้นหลักการทางเคมีในการคิด วิจัย วิเคราะห์ และแก้ปัญหา',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิตสาขาเทคโนโลยีสิ่งแวดล้อม และการจัดการอย่างยั่งยืน หลักสูตรปี พ.ศ.2565",
+        name: 'วิทยาศาสตรบัณฑิตสาขาเทคโนโลยีสิ่งแวดล้อม และการจัดการอย่างยั่งยืน หลักสูตรปี พ.ศ.2565',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/chemistry#/course_detail_tee_1"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/chemistry#/course_detail_tee_1',
           },
-        ]
+        ],
       },
       {
-        name: "หลักสูตรวิทยาศาสตรบัณฑิตสาขาวิชาเคมี วิศวกรรม และอุตสาหกรรม (หลักสูตรนานาชาติ)",
+        name: 'หลักสูตรวิทยาศาสตรบัณฑิตสาขาวิชาเคมี วิศวกรรม และอุตสาหกรรม (หลักสูตรนานาชาติ)',
         program: [
           {
-            name: "",
-            link: "https://inter.science.kmitl.ac.th/Bachelor_Degree/IEC"
+            name: '',
+            link: 'https://inter.science.kmitl.ac.th/Bachelor_Degree/IEC',
           },
-        ]
+        ],
       },
-    ]
+    ],
   },
   {
     logoPath: mathDepartment,
-    name: "ภาควิชาคณิตศาสตร์",
-    describe: "สร้างและพัฒนาองค์ความรู้ทางวิทยาศาสตร์ประยุกต์เพื่อให้ได้บุคลากรที่มีความรู้ความสามารถ",
+    name: 'ภาควิชาคณิตศาสตร์',
+    describe:
+      'สร้างและพัฒนาองค์ความรู้ทางวิทยาศาสตร์ประยุกต์เพื่อให้ได้บุคลากรที่มีความรู้ความสามารถ',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาคณิตศาสตร์ประยุกต์ หลักสูตรปี พ.ศ. 2565",
+        name: 'วิทยาศาสตรบัณฑิต สาขาคณิตศาสตร์ประยุกต์ หลักสูตรปี พ.ศ. 2565',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-math#/course_detail_tee_1"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-math#/course_detail_tee_1',
           },
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   {
     logoPath: physicDepartment,
-    name: "ภาควิชาฟิสิกส์",
-    describe: "มุ่งเน้นผลิตบัณฑิตเป็นนักคอมพิวเตอร์ที่สามารถวิเคราะห์ออกแบบและพัฒนาระบบงานทางด้านคอมพิวเตอร์โดยใช้เทคโนโลยีที่ทันสมัยและเหมาะสม",
+    name: 'ภาควิชาฟิสิกส์',
+    describe:
+      'มุ่งเน้นผลิตบัณฑิตเป็นนักคอมพิวเตอร์ที่สามารถวิเคราะห์ออกแบบและพัฒนาระบบงานทางด้านคอมพิวเตอร์โดยใช้เทคโนโลยีที่ทันสมัยและเหมาะสม',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิตสาขาวิชาฟิสิกส์อุตสาหกรรมหลักสูตรปี พ.ศ. 2564",
+        name: 'วิทยาศาสตรบัณฑิตสาขาวิชาฟิสิกส์อุตสาหกรรมหลักสูตรปี พ.ศ. 2564',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-physics#/course_detail_tee_2"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-physics#/course_detail_tee_2',
           },
-        ]
+        ],
       },
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาวิชาฟิสิกส์ประยุกต์ หลักสูตรปี พ.ศ. 2560",
+        name: 'วิทยาศาสตรบัณฑิต สาขาวิชาฟิสิกส์ประยุกต์ หลักสูตรปี พ.ศ. 2560',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-physics#/course_detail_tee_1"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-physics#/course_detail_tee_1',
           },
-        ]
+        ],
       },
-    ]
+    ],
   },
   {
     logoPath: bioDepartment,
-    name: "ภาควิชาชีววิทยา",
-    describe: "มุ่งเน้นผลิตบัณฑิต มีความรู้ใน ภาคทฤษฎีและมีทักษะปฏิบัติ อย่างมีคุณภาพ",
+    name: 'ภาควิชาชีววิทยา',
+    describe:
+      'มุ่งเน้นผลิตบัณฑิต มีความรู้ใน ภาคทฤษฎีและมีทักษะปฏิบัติ อย่างมีคุณภาพ',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาเทคโนโลยีชีวภาพอุตสาหกรรม หลักสูตรปี พ.ศ.2565",
+        name: 'วิทยาศาสตรบัณฑิต สาขาเทคโนโลยีชีวภาพอุตสาหกรรม หลักสูตรปี พ.ศ.2565',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-bio#/course_detail_tee_1"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-bio#/course_detail_tee_1',
           },
-        ]
+        ],
       },
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาวิชาจุลชีววิทยาอุตสาหกรรม หลักสูตรปี พ.ศ. 2560",
+        name: 'วิทยาศาสตรบัณฑิต สาขาวิชาจุลชีววิทยาอุตสาหกรรม หลักสูตรปี พ.ศ. 2560',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/assets/%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3/2560/%E0%B8%88%E0%B8%B8%E0%B8%A5%E0%B8%8A%E0%B8%B5%E0%B8%A7%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%AD%E0%B8%B8%E0%B8%95%E0%B8%AA%E0%B8%B2%E0%B8%AB%E0%B8%81%E0%B8%A3%E0%B8%A3%E0%B8%A1%202560.pdf"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/assets/%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3/2560/%E0%B8%88%E0%B8%B8%E0%B8%A5%E0%B8%8A%E0%B8%B5%E0%B8%A7%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%AD%E0%B8%B8%E0%B8%95%E0%B8%AA%E0%B8%B2%E0%B8%AB%E0%B8%81%E0%B8%A3%E0%B8%A3%E0%B8%A1%202560.pdf',
           },
-        ]
+        ],
       },
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาวิชาจุลชีววิทยาอุตสาหกรรม (นานาชาติ) หลักสูตรปี พ.ศ. 2559",
+        name: 'วิทยาศาสตรบัณฑิต สาขาวิชาจุลชีววิทยาอุตสาหกรรม (นานาชาติ) หลักสูตรปี พ.ศ. 2559',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-bio#/course_detail_tee_3"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-bio#/course_detail_tee_3',
           },
-        ]
+        ],
       },
-    ]
+    ],
   },
   {
     logoPath: comDepartment,
-    name: "ภาควิชาวิทยาการคอมพิวเตอร์",
-    describe: "มุ่งเน้นผลิตบัณฑิตเป็นนักคอมพิวเตอร์ที่สามารถวิเคราะห์ออกแบบและพัฒนาระบบงานทางด้านคอมพิวเตอร์โดยใช้เทคโนโลยีที่ทันสมัยและเหมาะสม",
+    name: 'ภาควิชาวิทยาการคอมพิวเตอร์',
+    describe:
+      'มุ่งเน้นผลิตบัณฑิตเป็นนักคอมพิวเตอร์ที่สามารถวิเคราะห์ออกแบบและพัฒนาระบบงานทางด้านคอมพิวเตอร์โดยใช้เทคโนโลยีที่ทันสมัยและเหมาะสม',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการคอมพิวเตอร์ (หลักสูตรปรับปรุง พ.ศ. 2564)",
+        name: 'วิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการคอมพิวเตอร์ (หลักสูตรปรับปรุง พ.ศ. 2564)',
         program: [
           {
-            name: "",
-            link: "https://www.reg.kmitl.ac.th/curriculum/file/bachelor/05/com_c2564edit.pdf"
+            name: '',
+            link: 'https://www.reg.kmitl.ac.th/curriculum/file/bachelor/05/com_c2564edit.pdf',
           },
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   {
     logoPath: statDepartment,
-    name: "ภาควิชาสถิติ",
-    describe: "ผลิตบุคลากรทางด้านสถิติที่มีความรู้ความสามารถทั้งในด้านทฤษฎี และการประยุกต์ในด้านสถิติการวิจัยดําเนินงาน และวิทยาการข้อมูล ตลอดจนสามารถนําเอาเทคโนโลยีที่ทันสมัย",
+    name: 'ภาควิชาสถิติ',
+    describe:
+      'ผลิตบุคลากรทางด้านสถิติที่มีความรู้ความสามารถทั้งในด้านทฤษฎี และการประยุกต์ในด้านสถิติการวิจัยดําเนินงาน และวิทยาการข้อมูล ตลอดจนสามารถนําเอาเทคโนโลยีที่ทันสมัย',
     curriculum: [
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาสถิติประยุกต์และการวิเคราะห์ข้อมูล หลักสูตรปี พ.ศ.2565",
+        name: 'วิทยาศาสตรบัณฑิต สาขาสถิติประยุกต์และการวิเคราะห์ข้อมูล หลักสูตรปี พ.ศ.2565',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/department-stat#/course_detail_tee_1"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/department-stat#/course_detail_tee_1',
           },
-        ]
+        ],
       },
       {
-        name: "วิทยาศาสตรบัณฑิต สาขาวิชาสถิติประยุกต์หลักสูตรปี พ.ศ. 2560",
+        name: 'วิทยาศาสตรบัณฑิต สาขาวิชาสถิติประยุกต์หลักสูตรปี พ.ศ. 2560',
         program: [
           {
-            name: "",
-            link: "https://www.science.kmitl.ac.th/assets/%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3/2560/%E0%B8%AA%E0%B8%96%E0%B8%B4%E0%B8%95%E0%B8%B4%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A2%E0%B8%B8%E0%B8%81%E0%B8%95%E0%B9%8C%202560.pdf"
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/assets/%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3/2560/%E0%B8%AA%E0%B8%96%E0%B8%B4%E0%B8%95%E0%B8%B4%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A2%E0%B8%B8%E0%B8%81%E0%B8%95%E0%B9%8C%202560.pdf',
           },
-        ]
+        ],
       },
-    ]
+    ],
+  },
+  {
+    logoPath: kdaiDepartment,
+    name: 'ศูนย์วิเคราะห์ข้อมูลดิจิทัลอัจฉริยะพระจอมเกล้าลาดกระบัง',
+    describe:
+      'มุ่งเน้นให้นักศึกษาเป็นนักวิทยาศาสตร์ข้อมูลที่มีความเชี่ยวชาญ อย่างลึกซึ้งทั้งทฤษฎีและปฏิบัติ',
+    curriculum: [
+      {
+        name: 'หลักสูตรวิทยาศาสตรบัณฑิต สาขาเทคโนโลยีดิจิทัลและนวัตกรรมเชิงบูรณาการ (หลักสูตรนานาชาติ)',
+        program: [
+          {
+            name: '',
+            link: 'https://www.science.kmitl.ac.th/course-files/kdai-bachelor.pdf',
+          },
+        ],
+      },
+    ],
   },
 ]
--- pages/about/ita.tsx
+++ pages/about/ita.tsx
@@ -9,12 +9,13 @@ const ItaPage = () => {
     <>
       <Container className="my-[16px]">
         <Breadcrumbs />
-        <p className="text-center font-bold mt-[16px] mb-3 text-xl">
+        <p className="mt-[16px] mb-3 text-center text-xl font-bold">
           ข้อมูลเปิดเผยสาธารณะ
         </p>
 
-        <div className="relative h-[500px] mb-5 text-center">
+        <div className="relative mb-5 h-[500px] text-center">
           <Image
+            className="object-contain"
             src="https://www.science.kmitl.ac.th/apiUpload/newsPicture/SXDxlCMaqTFD1640675066479/OFOrBHwZSTkK1640675066499.png"
             alt="science kmitl"
             fill
@@ -27,42 +28,50 @@ const ItaPage = () => {
           <br></br>&nbsp; &nbsp; 1.1 กฏหมายภายในสถาบัน
           <br></br>
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://law.kmitl.ac.th/#">
+          <a
+            target="_blank"
+            href="https://law.kmitl.ac.th/#"
+            rel="noreferrer noopener"
+          >
             สำนักงานนิติกร สำนักงานอธิการบดี&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; 1.2 คู่มือมาตรฐานการปฏิบัติงาน
           <br></br>
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/1.2.1 คู่มือพนักงานสถาบัน-กับสิ่งที่พนักงานสถาบันควรรู้ในการปฏิบัติงาน.pdf"
+            rel="noreferrer noopener"
           >
             คู่มือ มาตรฐานการปฏิบัติงาน
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/1.2.2 ประกาศชื่อตำแหน่งและมาตรฐานกำหนดตำแหน่งของพนักงานสถาบัน-พ.ศ.2557.pdf"
+            rel="noreferrer noopener"
           >
             ประกาศชื่อตำแหน่งและมาตรฐานกำหนดตำแหน่งของพนักงานสถาบัน พ..ศ.2557
-          </Link>
+          </a>
           <br></br>
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -
-          <Link
+          <a
             target="_blank"
             href="https://www.science.kmitl.ac.th/apiUpload/newsPicture/tLjEFGp7MxJ11641456573060/4cOF1UGj7fs41641456573080.pdf"
+            rel="noreferrer noopener"
           >
             ขั้นตอนการปฏิบัติงาน (work flow) ของหน่วยงานภายใน คณะวิทยาศาสตร์
-          </Link>
+          </a>
           <br></br> &nbsp; &nbsp; 1.3 คู่มือมาตรฐานการให้บริการ
           <div>
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-            <Link
+            <a
               target="_blank"
               href="http://www.science.kmitl.ac.th/page#/docs_container"
+              rel="noreferrer noopener"
             >
               เอกสารดาวน์โหลดสำหรับนักศึกษา
-            </Link>
+            </a>
           </div>
         </div>
         <br></br>
@@ -71,43 +80,48 @@ const ItaPage = () => {
           <b>2. การบริหารเงินงบประมาณปี</b>
           <br></br>&nbsp; &nbsp; 2.1 แผนยุทธศาสตร์หรือแผนพัฒนาหน่วยงาน
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/2.1.1 แผนปฏิบัติการสถาบัน 5 ปี (พ.ศ. 2560-2564)[1].pdf"
+            rel="noreferrer noopener"
           >
             แผนปฏิบัติการสถาบัน 5 ปี (พ.ศ. 2560-2564)
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/2.1.2 แผนกลยุทธ์สถาบัน 10 ปี (พ.ศ.2560-2570)[1].pdf"
+            rel="noreferrer noopener"
           >
             แผนกลยุทธ์สถาบัน 10 ปี (พ.ศ.2560-2570)
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; 2.2 แผนดำเนินงานประจำปี
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="https://www.science.kmitl.ac.th/apiUpload/newsPicture/XoeryVzLQtpa1643607747815/keLwokBGFauq1643607747831.pdf"
+            rel="noreferrer noopener"
           >
             เอกสารงบประมาณฉบับที่ 3 งบประมาณรายจ่าย ประจำปีงบประมาณ 2565 เล่มที่
             3(4)
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="https://www.science.kmitl.ac.th/apiUpload/newsPicture/sREYPXUHnVzM1640675583884/pbCIRDj4422c1640675583900.PDF"
+            rel="noreferrer noopener"
           >
             พรบ.งบประมาณรายจ่าย ประจำปีงบประมาณ พ.ศ. 2565
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; 2.3 การจัดซื้อจัดจ้าง
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/news_container/5b6c0262179eae3a4c17a1c5/5a8470b228d2e92a0c753010"
+            rel="noreferrer noopener"
           >
             ข้อมูลการจัดซื้อจัดจ้าง
-          </Link>
+          </a>
         </div>
         <br></br>
 
@@ -115,40 +129,55 @@ const ItaPage = () => {
           <b>3. การบริหารและพัฒนาทรัพยากรบุคคล</b>
           <br></br>&nbsp; &nbsp; &nbsp;3.1 โครงสร้างการแบ่งส่วนราชการของหน่วยงาน
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/structure"
+            rel="noreferrer noopener"
           >
             การแบ่งส่วนงานและการบริหารคณะวิทยาศาสตร์
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp;3.2 ข้อมูลผู้บริหาร
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/personel"
+            rel="noreferrer noopener"
           >
             ข้อมูลผู้บริหารคณะ&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://kmitl.ac.th/th/manager-kmitl/1">
+          <a
+            target="_blank"
+            href="http://kmitl.ac.th/th/manager-kmitl/1"
+            rel="noreferrer noopener"
+          >
             ข้อมูลผู้บริหารสถาบัน&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp;3.3 นโยบายการบริหาร
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/resolution"
+            rel="noreferrer noopener"
           >
             นโยบายการบริหารคณะวิทยาศาสตร์
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://kmitl.ac.th/th/vision">
+          <a
+            target="_blank"
+            href="http://kmitl.ac.th/th/vision"
+            rel="noreferrer noopener"
+          >
             วิสัยทัศน์ พันธกิจ ระดับสถาบัน&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://hr.kmitl.ac.th/?page_id=120">
+          <a
+            target="_blank"
+            href="http://hr.kmitl.ac.th/?page_id=120"
+            rel="noreferrer noopener"
+          >
             ข้อบังคับเกี่ยวกับการบริหารงานบุคคล&nbsp;
-          </Link>
+          </a>
         </div>
         <br></br>
 
@@ -156,51 +185,71 @@ const ItaPage = () => {
           <b>4. การใช้ทรัพย์สินของราชการ</b>
           <br></br>&nbsp; &nbsp; &nbsp; 4.1 การใช้ทรัพย์สินของราชการ
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://law.kmitl.ac.th/?p=2665">
+          <a
+            target="_blank"
+            href="https://law.kmitl.ac.th/?p=2665"
+            rel="noreferrer noopener"
+          >
             กฎระเบียบการบริหารการเงิน พัสดุ และทรัพย์สิน&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://www.pmo.kmitl.ac.th/#">
+          <a
+            target="_blank"
+            href="https://www.pmo.kmitl.ac.th/#"
+            rel="noreferrer noopener"
+          >
             สำนักงานบริหารทรัพย์สิน&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="https://www.itrector.kmitl.ac.th/vehicle/"
+            rel="noreferrer noopener"
           >
             ระบบการจองรถส่วนกลาง&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/docs_container"
+            rel="noreferrer noopener"
           >
             แบบฟอร์มการให้บริการงานเทคโนโลยีสารสนเทศ
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://csc.kmitl.ac.th/cscweb-1/">
+          <a
+            target="_blank"
+            href="https://csc.kmitl.ac.th/cscweb-1/"
+            rel="noreferrer noopener"
+          >
             สำนักบริการคอมพิวเตอร์&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://www.lib.kmitl.ac.th/">
+          <a
+            target="_blank"
+            href="http://www.lib.kmitl.ac.th/"
+            rel="noreferrer noopener"
+          >
             สำนักหอสมุดกลาง&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%81%E0%B8%B2%E0%B8%A8%E0%B8%A1%E0%B8%B2%E0%B8%95%E0%B8%A3%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%9B%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%8A%E0%B9%89%E0%B8%97%E0%B8%A3%E0%B8%B1%E0%B8%9E%E0%B8%A2%E0%B9%8C%E0%B8%AA%E0%B8%B4%E0%B8%99%E0%B8%82.pdf"
+            rel="noreferrer noopener"
           >
             ประกาศคณะวิทยาศาสตร์
             เรื่องมาตรการป้องกันกรณีการใช้ทรัพย์สินของสถาบัน
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%81%E0%B8%B2%E0%B8%A8%E0%B9%80%E0%B8%9C%E0%B8%A2%E0%B9%81%E0%B8%9E%E0%B8%A3%E0%B9%88%20%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%9A%E0%B8%B4%E0%B8%81%E0%B8%88%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B8%A7%E0%B8%B1%E0%B8%AA%E0%B8%94%E0%B8%B8-%E0%B8%84%E0%B8%A3%E0%B8%B8%E0%B8%A0%E0%B8%B1%E0%B8%93%E0%B8%91%E0%B9%8C%E0%B8%AF.pdf"
+            rel="noreferrer noopener"
           >
             ประกาศคณะวิทยาศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
             เรื่อง เผยแพร่ การเบิกจ่ายวัสดุ-ครุภัณฑ์ และการยืมคืนครุภัณฑ์
-          </Link>
+          </a>
         </div>
         <br></br>
 
@@ -208,33 +257,40 @@ const ItaPage = () => {
           <b>5. การส่งเสริมความโปร่งใสในหน่วยงาน</b>
           <br></br>&nbsp; &nbsp; &nbsp; 5.1 การแก้ไขปัญหาการทุจริต
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="https://www.science.kmitl.ac.th/apiUpload/newsPicture/IztGwo9Q1Khf1640675443114/iOFLGCzbwBXi1640675443139.pdf"
+            rel="noreferrer noopener"
           >
             คณะกรรมการขับเคลื่อนการประเมินคุณธรรมและความโปร่งใสในการดำเนินงานของหน่วยงานภาครัฐ
             (ITA) คณะวิทยาศาสตร์
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="https://www.science.kmitl.ac.th/apiUpload/newsPicture/nBxQnnlzCfUs1640675734365/PhVJn8oIcB861640675734467.pdf"
+            rel="noreferrer noopener"
           >
             คู่มือการประเมินคุณธรรมและความโปร่งใสในการดำเนินงาน
             ของหน่วยงานภาครัฐ ประจำปีงบประมาณ พ.ศ.2565&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/upload/news_img/5.1.2เอกสารรายละเอียดการประเมิน ITA 2563.pdf"
+            rel="noreferrer noopener"
           >
             แนวทางการยกระดับค่าคะแนนการประเมินคุณธรรมและความโปร่งใสในการดำเนินงานของหน่วยงานภาครัฐ
             (ITA)&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://law.kmitl.ac.th/?page_id=3425">
+          <a
+            target="_blank"
+            href="https://law.kmitl.ac.th/?page_id=3425"
+            rel="noreferrer noopener"
+          >
             มาตรการส่งเสริมคุณธรรมและความโปร่งใสภายในหน่วยงาน
-          </Link>
+          </a>
         </div>
         <br></br>
 
@@ -242,17 +298,29 @@ const ItaPage = () => {
           <b>6. ข่าวประชาสัมพันธ์</b>
           <br></br>&nbsp; &nbsp; &nbsp; 6.1 ข่าวประชาสัมพันธ์
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://www.science.kmitl.ac.th">
+          <a
+            target="_blank"
+            href="http://www.science.kmitl.ac.th"
+            rel="noreferrer noopener"
+          >
             เว็บไซต์คณะวิทยาศาสตร์
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://www.facebook.com/SciKmitl">
+          <a
+            target="_blank"
+            href="https://www.facebook.com/SciKmitl"
+            rel="noreferrer noopener"
+          >
             Facebook คณะวิทยาศาสตร์&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://kmitl.ac.th/th">
+          <a
+            target="_blank"
+            href="http://kmitl.ac.th/th"
+            rel="noreferrer noopener"
+          >
             เว็บไซต์สถาบัน
-          </Link>
+          </a>
         </div>
         <br></br>
 
@@ -260,23 +328,31 @@ const ItaPage = () => {
           <b>7. บริการ</b>
           <br></br>&nbsp; &nbsp; &nbsp; 7.1 ช่องทางการรับฟังความคิดเห็น
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="http://www.science.kmitl.ac.th/#/home">
+          <a
+            target="_blank"
+            href="'https://www.science.kmitl.ac.th/page#/contact'"
+          >
             ข้อมูลการติดต่อ&nbsp;
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link
+          <a
             target="_blank"
             href="http://www.science.kmitl.ac.th/page#/dean"
+            rel="noreferrer noopener"
           >
             ติดต่อร้องเรียน ระดับคณะ
-          </Link>
+          </a>
           <br></br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -{' '}
-          <Link target="_blank" href="https://osda.science.kmitl.ac.th/">
+          <a
+            target="_blank"
+            href="https://osda.science.kmitl.ac.th/"
+            rel="noreferrer noopener"
+          >
             บริการถามตอบปัญหา
-          </Link>
+          </a>
         </div>
 
-        <div className="relative h-[500px] mt-5 mb-5 text-center">
+        <div className="relative mt-5 mb-5 h-[500px] text-center">
           <Image
             css={{
               objectFit: 'contain',
@@ -292,7 +368,7 @@ const ItaPage = () => {
       <style jsx scoped>{`
         a {
           font-size: inherit !important;
-          color: #fd600e;
+          color: #fd600e !important;
         }
 
         a:hover {
