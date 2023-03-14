ca1bd0f80d3e84c6ad4cd4e094bcd5cbc67b89bd
Siwakorn <cardeer.dev@gmail.com>
2023-03-14 20:45:54 +0700

    feat: hide credits

--- local_components/index/TutitionFeeCard.tsx
+++ local_components/index/TutitionFeeCard.tsx
@@ -1,56 +1,75 @@
 import { ITutitionFeeCard, ITutitionFeeCard2 } from '@/types/tutition-fee'
 import Image from 'next/image'
 
-
-export const TutitionFeeCard1 = ({ icon, department, branch }: ITutitionFeeCard) => {
+export const TutitionFeeCard1 = ({
+  icon,
+  department,
+  branch,
+}: ITutitionFeeCard) => {
   return (
-    <div className='flex lg:gap-4 mx-2 mb-4 py-2 md:py-4 lg:py-6 border bg-white border-[#E1E1E1] rounded-xl'>
-      <div className='relative w-16 md:w-24 h-16 md:h-24'>
-        <Image src={icon} alt='icon' fill sizes="(min-width: 0) 100vw" />
+    <div className="mx-2 mb-4 flex rounded-xl border border-[#E1E1E1] bg-white py-2 md:py-4 lg:gap-4 lg:py-6">
+      <div className="relative h-16 w-16 md:h-24 md:w-24">
+        <Image src={icon} alt="icon" fill sizes="(min-width: 0) 100vw" />
       </div>
-      <div className='flex-1 body1 lg:h6 pr-1'>
-        <h4 className='bodyBig lg:text-[30px] font-bold mb-2'>{department}</h4>
+      <div className="body1 lg:h6 flex-1 pr-1">
+        <h4 className="bodyBig mb-2 font-bold lg:text-[30px]">{department}</h4>
         {branch.map((val, i) => {
           return (
-            <div className='ml-2 mb-1' key={val.name + i}>
-              <p className='font-bold mb-1'>{val.name}</p>
-              <div className='flex flex-col md:flex-row md:gap-16 body2 lg:bodyBig'>
-                <p>ค่าธรรมเนียมการศึกษา : {val.tutitionFee} บาท ต่อภาคการศึกษา</p>
-                <p>จำนวนหน่วยกิต : {val.credit}</p>
+            <div className="ml-2 mb-1" key={val.name + i}>
+              <p className="mb-1 font-bold">{val.name}</p>
+              <div className="body2 lg:bodyBig flex flex-col md:flex-row md:gap-16">
+                <p>
+                  ค่าธรรมเนียมการศึกษา : {val.tutitionFee} บาท ต่อภาคการศึกษา
+                </p>
+                {/* <p>จำนวนหน่วยกิต : {val.credit}</p> */}
               </div>
-            </div>)
+            </div>
+          )
         })}
       </div>
     </div>
   )
 }
-export const TutitionFeeCard2 = ({ icon, department, branch }: ITutitionFeeCard2) => {
+export const TutitionFeeCard2 = ({
+  icon,
+  department,
+  branch,
+}: ITutitionFeeCard2) => {
   return (
-    <div className='flex lg:gap-4 mx-2 mb-4 py-2 md:py-4 lg:py-6 border bg-white border-[#E1E1E1] rounded-xl'>
-      <div className='relative w-16 md:w-24 h-16 md:h-24'>
-        <Image src={icon} alt='icon' fill sizes="(min-width: 0) 100vw" />
+    <div className="mx-2 mb-4 flex rounded-xl border border-[#E1E1E1] bg-white py-2 md:py-4 lg:gap-4 lg:py-6">
+      <div className="relative h-16 w-16 md:h-24 md:w-24">
+        <Image src={icon} alt="icon" fill sizes="(min-width: 0) 100vw" />
       </div>
-      <div className='flex-1 body1 lg:h6 pr-1'>
-        <h4 className='bodyBig lg:text-[30px] font-bold mb-2'>{department}</h4>
+      <div className="body1 lg:h6 flex-1 pr-1">
+        <h4 className="bodyBig mb-2 font-bold lg:text-[30px]">{department}</h4>
         {branch.map((val, i) => {
           return (
-            <div className='ml-2 mb-1' key={val.name + i}>
-              <p className='font-bold'>{val.name}</p>
+            <div className="ml-2 mb-1" key={val.name + i}>
+              <p className="font-bold">{val.name}</p>
               {val.lessonPlan.map((val, i) => {
                 return (
                   <div key={val.name}>
-                    <p className='text-[#6A6A6A] font-bold lg:mt-2'>{val.name}</p>
-                    <div className='flex flex-col md:flex-row md:gap-16 body2 lg:bodyBig'>
+                    <p className="font-bold text-[#6A6A6A] lg:mt-2">
+                      {val.name}
+                    </p>
+                    <div className="body2 lg:bodyBig flex flex-col md:flex-row md:gap-16">
                       <div>
-                        <p>ค่าธรรมเนียมการศึกษา นักศึกษาสามัญ   : {val.tutitionFee1} บาท ต่อภาคการศึกษา</p>
-                        <p>ค่าธรรมเนียมการศึกษา นักศึกษาต่างชาติ : {val.tutitionFee2} บาท ต่อภาคการศึกษา</p>
+                        <p>
+                          ค่าธรรมเนียมการศึกษา นักศึกษาสามัญ :{' '}
+                          {val.tutitionFee1} บาท ต่อภาคการศึกษา
+                        </p>
+                        <p>
+                          ค่าธรรมเนียมการศึกษา นักศึกษาต่างชาติ :{' '}
+                          {val.tutitionFee2} บาท ต่อภาคการศึกษา
+                        </p>
                       </div>
-                      <p>จำนวนหน่วยกิต : {val.credit}</p>
+                      {/* <p>จำนวนหน่วยกิต : {val.credit}</p> */}
                     </div>
                   </div>
                 )
               })}
-            </div>)
+            </div>
+          )
         })}
       </div>
     </div>
