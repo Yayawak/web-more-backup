e6d319e6960db5c8e4c34f3b58c997d5250d331d
Saint99599 <saint99599@gmail.com>
2023-06-03 20:04:27 +0700

    Add eta link to faculty website

--- local_components/index/Partners.tsx
+++ local_components/index/Partners.tsx
@@ -1,5 +1,6 @@
 import Container from '@/components/Layout/Container'
 import Image from 'next/image'
+import Link from 'next/link'
 import IndexSectionTitle from './SectionTitle'
 import partnerEtat from '@/assets/images/partners/etat.png'
 import partnerIsp from '@/assets/images/partners/isp.png'
@@ -12,13 +13,15 @@ const IndexPartners = () => {
       <IndexSectionLine className="mt-14">Our Partners</IndexSectionLine>
       <Container>
         <div className="relative mx-auto mt-10 h-[143px]">
-          <Image
-            className="object-contain"
-            src={partnerEtat.src}
-            alt="etat"
-            fill
-            sizes="(min-width: 0) 100vw"
-          />
+          <Link href={"https://etat.informatics.buu.ac.th/"}>
+            <Image
+              className="object-contain"
+              src={partnerEtat.src}
+              alt="etat"
+              fill
+              sizes="(min-width: 0) 100vw"
+            />
+          </Link>
         </div>
 
         <IndexSectionLine className="mt-4"></IndexSectionLine>
